5050cb157e2d0f93922eec69174f3786a8f3c858
heba mostafa <96655351+hebamost@users.noreply.github.com>
2022-05-12 23:00:14 +0200

    Update synch.h

--- thread/synch.h
+++ thread/synch.h
@@ -22,6 +22,8 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    int priority ;   /* priority of thread that holds the lock or max priority of threading waiting for it. */
+    struct list_elem elem; /* List element. */
   };
 
 void lock_init (struct lock *);
