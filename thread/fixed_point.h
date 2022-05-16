f3ed00631db176b231c77f4e9b14caa42d833cdb
Engyyyy <81655040+Engyyyy@users.noreply.github.com>
2022-05-16 03:02:37 +0200

    add multiply real and real function prototype

--- thread/fixed_point.h
+++ thread/fixed_point.h
@@ -13,6 +13,7 @@ Real add_real_to_real(Real num1, Real num2);
 Real subtract_int_from_real(Real real_number, int integer);
 Real subtract_real_from_real(Real num1, Real num2);
 Real multiply_real_and_int(Real real_number, int integer);
+Real multiply_real_and_real(Real num1, Real num2);
 Real divide_real_by_int(Real real_number, int integer);
 Real divide_real_by_real(Real num1, Real num2);
 
