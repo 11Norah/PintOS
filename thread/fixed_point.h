40c8fb1067bfb3b3e6963738a7a020152d7b431d
Engyyyy <81655040+Engyyyy@users.noreply.github.com>
2022-05-16 04:21:52 +0200

    Update fixed_point.h

--- thread/fixed_point.h
+++ thread/fixed_point.h
@@ -1,20 +1,94 @@
-
 #ifndef PINTOS_FIXED_POINT_H
 #define PINTOS_FIXED_POINT_H
+
+#define INTEGER_BITS 17
+#define DECIMAL_BITS 14
+
 typedef struct real {
     int value;
 } Real;
 
-Real convert_to_real(int integer);
-int convert_to_integer(Real real_number);
-int round_real_to_int(Real num);
-Real add_real_to_int(Real real_number, int integer);
-Real add_real_to_real(Real num1, Real num2);
-Real subtract_int_from_real(Real real_number, int integer);
-Real subtract_real_from_real(Real num1, Real num2);
-Real multiply_real_and_int(Real real_number, int integer);
-Real multiply_real_and_real(Real num1, Real num2);
-Real divide_real_by_int(Real real_number, int integer);
-Real divide_real_by_real(Real num1, Real num2);
+inline Real convert_to_real(int integer)
+{
+    Real real_number;
+    real_number.value = integer << DECIMAL_BITS;
+    return real_number;
+}
+
+inline int convert_to_integer(Real real_number)
+{
+    return real_number.value >> DECIMAL_BITS;
+}
+
+inline int round_real_to_int(Real num)
+{
+    int whole = num.value / (1 << DECIMAL_BITS);
+    int decimal = num.value - (whole << DECIMAL_BITS);
+    if(decimal >= (1 << (DECIMAL_BITS-1))) return whole + 1;
+    else return whole;
+}
+
+inline Real add_real_to_int(Real real_number, int integer)
+{
+    Real sum;
+    sum.value = real_number.value + convert_to_real(integer).value;
+    return sum;
+}
+
+inline Real add_real_to_real(Real num1, Real num2)
+{
+    Real sum;
+    sum.value = num1.value + num2.value;
+    return sum;
+}
+
+inline Real subtract_int_from_real(Real real_number, int integer)
+{
+    Real diff;
+    diff.value = real_number.value - convert_to_real(integer).value;
+    return diff;
+}
+
+inline Real subtract_real_from_real(Real num1, Real num2)
+{
+    Real diff;
+    diff.value = num1.value - num2.value;
+    return diff;
+}
+
+inline Real multiply_real_and_int(Real real_number, int integer)
+{
+    Real prod;
+    prod.value = real_number.value * integer;
+    return prod;
+}
+
+inline Real multiply_real_and_real(Real num1, Real num2)
+{
+    Real prod;
+    prod.value = (num1.value * num2.value) >> DECIMAL_BITS;
+    return prod;
+}
+
+inline Real divide_real_by_int(Real real_number, int integer)
+{
+    Real quotient;
+    quotient.value = real_number.value / integer;
+    return quotient;
+}
+
+inline Real divide_real_by_real(Real num1, Real num2)
+{
+    Real quotient;
+    quotient.value = (num1.value << DECIMAL_BITS) / num2.value;
+    return quotient;
+}
 
 #endif //PINTOS_FIXED_POINT_H
+
+
+
+
+
+
+
