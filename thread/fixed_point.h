14c034a84a9b40767fab204381c53d150e64f5a2
Engyyyy <81655040+Engyyyy@users.noreply.github.com>
2022-05-17 21:17:33 +0200

    Update fixed_point.h

--- thread/fixed_point.h
+++ thread/fixed_point.h
@@ -24,8 +24,15 @@ inline int round_real_to_int(Real num)
 {
     int whole = num.value / (1 << DECIMAL_BITS);
     int decimal = num.value - (whole << DECIMAL_BITS);
-    if(decimal >= (1 << (DECIMAL_BITS-1))) return whole + 1;
-    else return whole;
+    if(num.value >= 0)
+    {
+        if (decimal >= (1 << (DECIMAL_BITS - 1))) whole = whole + 1;
+    }
+    else
+    {
+        if (decimal >= (1 << (DECIMAL_BITS - 1))) whole = whole - 1;
+    }
+    return whole;
 }
 
 inline Real add_real_to_int(Real real_number, int integer)
@@ -66,7 +73,7 @@ inline Real multiply_real_and_int(Real real_number, int integer)
 inline Real multiply_real_and_real(Real num1, Real num2)
 {
     Real prod;
-    prod.value = (num1.value * num2.value) >> DECIMAL_BITS;
+    prod.value = (((int64_t)num1.value) * num2.value) >> DECIMAL_BITS;
     return prod;
 }
 
@@ -80,15 +87,8 @@ inline Real divide_real_by_int(Real real_number, int integer)
 inline Real divide_real_by_real(Real num1, Real num2)
 {
     Real quotient;
-    quotient.value = (num1.value << DECIMAL_BITS) / num2.value;
+    quotient.value = (((int64_t)num1.value) << DECIMAL_BITS) / num2.value;
     return quotient;
 }
 
 #endif //PINTOS_FIXED_POINT_H
-
-
-
-
-
-
-
