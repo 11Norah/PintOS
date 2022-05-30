844affa9e3b263b9034c6dce1983b94fbb75bc14
Norah Ahmed <95590883+11Norah@users.noreply.github.com>
2022-05-30 20:51:10 +0200

    Update syscall.h

--- Phase2/Userprog/syscall.h
+++ Phase2/Userprog/syscall.h
@@ -1,10 +1,29 @@
 
 
+
+
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
+struct lock write_lock;
+int File_Remover(char * file_name);
 
+//wrapper part
+void WrapperWrite(struct intr_frame *f);
+void WrapperRead(struct intr_frame *f);
+
+void WrapperOpen(struct intr_frame *f);
+void WrapperClose(struct intr_frame *f);
+
+void WrapperCreate(struct intr_frame *f);
+void WrapperExit(struct intr_frame *f);
+
+void WrapperWait(struct intr_frame *f);
+void WrapperExec(struct intr_frame *f);
+
+void WrapperRemove(struct intr_frame *f);
 #endif /* userprog/syscall.h */
 
   
+
